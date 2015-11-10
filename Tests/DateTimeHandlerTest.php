be4bf74164ba734a99dc845fd7843417e40560d4
kipal42 <kipal42@gmail.com>
2015-11-10 23:30:50 +0100

    require fix for travis.

--- Tests/DateTimeHandlerTest.php
+++ Tests/DateTimeHandlerTest.php
@@ -5,8 +5,7 @@
  * license that can be found in the LICENSE file.
  */
 
-/*require_once '../DateTimeHandler.php';
-require_once '../TimeSlot.php';*/
+//require_once '../DateTimeHandler.php';
 require_once 'TestTimeSlot.php';
 
 /**
--- Tests/TestTimeSlot.php
+++ Tests/TestTimeSlot.php
@@ -5,6 +5,8 @@
  * license that can be found in the LICENSE file.
  */
 
+require_once '../TimeSlot.php';
+
 class TestTimeSlot implements TimeSlot
 {
 	private $startDate = null;
